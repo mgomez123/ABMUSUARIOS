1537f9d2c44faa0e5f06b92fd33853e120081e9d
mgomez123 <gomezmicaela123@gmail.com>
2024-05-31 11:57:14 -0300

    Agregar archivos de proyecto.

---
+++ Data/AbmusuariosContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABMUSUARIOS.Data;
+
+public partial class AbmusuariosContext : DbContext
+{
+    public AbmusuariosContext()
+    {
+    }
+
+    public AbmusuariosContext(DbContextOptions<AbmusuariosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC274EB6A2AE");
+
+            entity.HasIndex(e => e.Email, "UQ__Usuarios__161CF724628EF84D").IsUnique();
+
+            entity.HasIndex(e => e.Usuario1, "UQ__Usuarios__E251F484C9B634CC").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("APELLIDO");
+            entity.Property(e => e.Ciudad)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("CIUDAD");
+            entity.Property(e => e.CodigoPostal)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("CODIGO_POSTAL");
+            entity.Property(e => e.Contraseña)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("CONTRASEÑA");
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("DIRECCION");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("EMAIL");
+            entity.Property(e => e.FechaNac)
+                .HasColumnType("date")
+                .HasColumnName("FECHA_NAC");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("NOMBRE");
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("TELEFONO");
+            entity.Property(e => e.Usuario1)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("USUARIO");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
